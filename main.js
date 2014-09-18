d0fc50c70b4fb972f9196606160ccecc134df1d9
Adam Weiner <adamwineguy@gmail.com>
2014-09-18 11:31:23 -0600

    finished popup jquery exercise

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 $(document).on('ready', function() {
-$('p').css('visibility', 'hidden');
-$('#popup').dialog({autoOpen: false});
-$('button').click(function() {
-	$('#popup').dialog('open');
-});
+	$('p').css('visibility', 'hidden');
+	$('button').click(function() {
+		$('body').append('<div class="popup"><p>Lorem ipsum dolor sit amet, consectetur  elit. Consequuntur quae fugiat praesentium voluptate? Minus eius, dicta sint facere voluptate dolores ad harum tempora animi architecto molestiae obcaecati, quas repellendus alias?</p><button class="pbutton">Close</button></div>');
+		$('.pbutton').click(function() {
+			$('.popup').remove();
+		});
+	});
+
+	
+	
 });
